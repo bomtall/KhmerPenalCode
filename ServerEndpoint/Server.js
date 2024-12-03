a2b5d1476dc2b5e98ee6f7dd3ba90a258d9b33bf
John Gaskell <john.liam.gaskell@gmail.com>
2024-12-03 21:49:28 +0000

    Base requests needed for MVP completed

--- ServerEndpoint/Server.js
+++ ServerEndpoint/Server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
 
-
 const app = express();
 const port = 3000;
 
@@ -9,11 +8,11 @@ const connectionConfig = {
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database: 'kmherpenalcode'
+    database: 'khmerpenalcode'
 };
 
 //Here we create a connection pool to help with the connection to the database
-var pool = mysql.createPool(connectionConfig);
+let pool = mysql.createPool(connectionConfig);
 
 
 //We also allow the application to use json as the data format
@@ -24,25 +23,29 @@ app.listen(port, function (err) {
     if (err) {
         console.log("Error in server setup");
     } else {
-        console.log(`Server running on port ${port}`);
+        console.log(`Server running on port`, port);
     }
 });
 
 // Define a GET endpoint to query the database
-app.get('/get-crimes', (req, res) => {
-    var ID = req.query.ID;
-
+app.get('/GetCrimes', (req, res) => {
     pool.getConnection(function (err, connection) {
         if (err) {
-            console.log(err);
+            console.log("Error: " + err);
+            connection.release();
+            return res.status(500).send(err);
         }
 
-        let sql = 'SELECT * FROM crime'; // Replace 'your_table_name' with your actual table name
+        let sql = 'SELECT * FROM crime';
+
         connection.query(sql, function (err, results) {
             if (err) {
                 console.log("Error: " + err);
                 connection.release();
                 return res.status(500).send(err);
+            }
+            if (results.length == 0) {
+                return res.status(404).send("Crimes not found");
             }
             res.status(200).json(results);
             connection.release();
@@ -50,25 +53,119 @@ app.get('/get-crimes', (req, res) => {
     });
 });
 
-
-app.get('/get-aggrivation-and-clauses-From-crime', (req, res) => {
-    var ID = req.query.ID;
-    let sql = 'SELECT * FROM aggrivations WHERE CrimeID = ID'; // Replace 'your_table_name' with your actual table name
-    db.query(sql, (err, results) => {
+app.get('/GetCrime', (req, res) => {
+    if (req.query.ID == null) {
+        return res.status(400).send("ID is required");
+    }
+    pool.getConnection(function (err, connection) {       
         if (err) {
+            console.log("Error: " + err);
+            connection.release();
             return res.status(500).send(err);
         }
-        res.json(results);
+
+        let sql = `SELECT * FROM crime WHERE ID = ${req.query.ID}`;
+
+        connection.query(sql, function (err, results) {
+            if (err) {
+                console.log("Error: " + err);
+                connection.release();
+                return res.status(500).send(err);
+            }
+            if (results.length == 0) {
+                return res.status(404).send("Crimes not found");
+            }
+            res.status(200).json(results[0]);
+            connection.release();
+        });
     });
 });
 
-app.get('/get-article-and-clauses', (req, res) => {
+app.get('/GetAggrivationAndClausesFromCrime', (req, res) => {
     var ID = req.query.ID;
-    let sql = `SELECT * FROM articles WHERE ID = ${ID}`; // Replace 'your_table_name' with your actual table name
-    db.query(sql, (err, results) => {
+    if (ID == null) {
+        return res.status(400).send("Crime ID is required");
+    }
+
+    pool.getConnection(function (err, connection) {
         if (err) {
-            return res.status(500).send(err);
+            console.log("Error: " + err);
+            connection.release();
+            return res.status(500)
         }
-        res.json(results);
+        let sql = `SELECT * FROM aggrivations INNER JOIN clauses ON aggrivations.ClauseID = clauses.ClauseId WHERE CrimeID = ${ID}`;
+
+        connection.query(sql, (err, results) => {
+            if (err) {
+                console.log("Error: " + err);
+                connection.release();
+                return res.status(500).send(err);
+            }
+            if (results.length == 0) {
+                return res.status(404).send("Aggrivations not found");
+            }
+            res.status(200).json(results);
+            connection.release();
+        });
+    });
+});
+
+app.get('/GetArticleAndClauses', (req, res) => {
+    var ID = req.query.Article;
+    if (ID == null) {
+        return res.status(400).send("Article is required");
+    }
+
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            console.log("Error: " + err);
+            connection.release();
+            return res.status(500)
+        }
+
+        let sql = `SELECT *, (SELECT JSON_ARRAYAGG(JSON_OBJECT('English',clauses.ClauseEnglish,'Khmer',clauses.ClauseKhmer,'ID',clauses.ClauseId)) FROM Clauses WHERE clauses.ArticleNumber = articles.ArticleNumber) AS Clauses FROM articles WHERE articles.ArticleNumber = ${ID}`;
+
+        connection.query(sql, (err, results) => {
+            if (err) {
+                console.log("Error: " + err);
+                connection.release();
+                return res.status(500).send(err);
+            }
+            if (results.length == 0) {
+                return res.status(404).send("Article not found");
+            }
+            results[0].Clauses = JSON.parse(results[0].Clauses);
+            res.status(200).json(results[0]);
+            connection.release();
+        });
+    });
+});
+
+app.get('/GetAllArticlesAndClauses', (req, res) => {
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            console.log("Error: " + err);
+            connection.release();
+            return res.status(500)
+        }
+
+        let sql = `SELECT *, (SELECT JSON_ARRAYAGG(JSON_OBJECT('English',clauses.ClauseEnglish,'Khmer',clauses.ClauseKhmer,'ID',clauses.ClauseId)) FROM Clauses WHERE clauses.ArticleNumber = articles.ArticleNumber) AS Clauses FROM articles`;
+
+        connection.query(sql, (err, results) => {
+            if (err) {
+                console.log("Error: " + err);
+                connection.release();
+                return res.status(500).send(err);
+            }
+            if (results.length == 0) {
+                return res.status(404).send("Articles not found");
+            }
+
+            for (let i = 0; i < results.length; i++) {
+                results[i].Clauses = JSON.parse(results[i].Clauses);
+            }
+            res.status(200).json(results);
+            connection.release();
+        });
     });
 });
